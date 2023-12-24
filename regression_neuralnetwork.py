aeba39d84db3212f0ddbee2c7496ca45adc5b4a1
Aryaveer <149290512+Veer376@users.noreply.github.com>
2023-12-24 16:55:35 +0530

    Add files via upload
    Minimum loss by playing with the hyperparameters, learning rate changed from 0.001(Default) to 1.

--- regression_neuralnetwork.py
+++ regression_neuralnetwork.py
@@ -1,34 +1,39 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+tf.random.set_seed(42)
+np.random.seed(42)
+x = np.array([2, 3, 4, 5, 6, 7, 8], dtype=float)
+y = np.array([65, 75, 80, 82, 88, 92, 95], dtype=float)
 
-# Creating the dataset
-x_train = np.array([2, 3, 4, 5, 6, 7, 8], dtype=float)
-y_train = np.array([65, 75, 80, 82, 88, 92, 95], dtype=float)
+model = tf.keras.Sequential() # creating the stack of layers inside the model variable
 
-# Creating a Sequential model
-model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(units=1, input_shape=[1])) # units is the number of neuron and input shape is the dimensionality of the input
+custom_optimizer = tf.keras.optimizers.Adam(learning_rate=1)
 
-# Adding a single neuron Dense layer for linear regression
-model.add(tf.keras.layers.Dense(units=1, input_shape=[1]))
+model.compile(optimizer=custom_optimizer, loss="mean_squared_error")
 
-# Compile the model
-model.compile(optimizer='adam', loss='mean_squared_error')
+history = model.fit(x, y, epochs=500, verbose=0)
 
-# Training the model
-history = model.fit(x_train, y_train, epochs=500, verbose=0)
+X = np.array([3.5, 4.5, 9.5], dtype=float)
+predictions = model.predict(X)
 
-# Predicting exam scores for some study hours
-x_test = np.array([3.5, 4.5, 9.5], dtype=float)
-predictions = model.predict(x_test)
+for i in range(len(X)):
+    print(f"For {X[i]} hours, predicted score: {predictions[i][0]}")
 
-# Printing predicted scores
-for i in range(len(x_test)):
-    print(f"For {x_test[i]} hours, predicted score: {predictions[i][0]}")
 
-# Plotting the training loss
-plt.plot(history.history['loss'])
-plt.title('Model Loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
+intercept = model.layers[0].get_weights()[1]
+slope = model.layers[0].get_weights()[0]
+
+print("Intercept:", intercept)
+print("Slope:", slope)
+
+plt.scatter(x,y,color="green")
+
+# plt.plot(history.history['loss'])
+# plt.title('Model Loss')
+# plt.xlabel('Epochs')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.plot(x,model.predict(x),color="red")
 plt.show()
